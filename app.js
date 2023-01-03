7515d480462b3ddd78731c8e5f041e5f0e7d3bc0
Marcello Donisi <marce.donisi@studenti.unina.it>
2023-01-03 17:25:04 +0100

    add logout btn

--- app.js
+++ app.js
@@ -1,60 +1,78 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
 
-const errorController = require('./controllers/error');
-const User = require('./models/user');
+const errorController = require('./controllers/error')
+const User = require('./models/user')
 
-const app = express();
+const MONGODB_URI =
+  'mongodb+srv://ildecimo_node:Yqk3cdHnWRypqK7O@ildecimo.f8r39ia.mongodb.net/app'
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const app = express()
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+})
 
-const adminRoutes = require('./routes/admin');
-const userRoutes = require('./routes/user');
-const authRoutes = require('./routes/auth');
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+const adminRoutes = require('./routes/admin')
+const userRoutes = require('./routes/user')
+const authRoutes = require('./routes/auth')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+  session({
+    secret: 'my secret', //TODO: cambiare il segreto con una stringa molto lunga e complicata
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    cookie: { 
+      maxAge: 100000 //si puo anche togliere
+    } 
+  }),
+) 
 
 app.use((req, res, next) => {
   User.findById('5baa2528563f16379fc8a610')
-    .then(user => {
-      req.user = user;
-      next();
+    .then((user) => {
+      req.user = user
+      next()
     })
-    .catch(err => console.log(err));
-});
+    .catch((err) => console.log(err))
+})
 
-app.use('/admin', adminRoutes);
-app.use(userRoutes);
-app.use(authRoutes);
+app.use('/admin', adminRoutes)
+app.use(userRoutes)
+app.use(authRoutes)
 
-app.use(errorController.get404);
+app.use(errorController.get404)
 
 mongoose
-  .connect(
-    'mongodb+srv://ildecimo_node:Yqk3cdHnWRypqK7O@ildecimo.f8r39ia.mongodb.net/app?retryWrites=true&w=majority'
-  )
-  .then(result => {
-    User.findOne().then(user => {
+  .connect(MONGODB_URI)
+  .then((result) => {
+    User.findOne().then((user) => {
       if (!user) {
         const user = new User({
-      name: 'Claudio',
-      email: 'claudio@test.com',
-      matcheslist: {
-        matches: []
-      }
-    });
-    user.save();
+          name: 'Claudio',
+          email: 'claudio@test.com',
+          matcheslist: {
+            matches: [],
+          },
+        })
+        user.save()
       }
     })
-    
-    app.listen(3000);
+
+    app.listen(3000)
     console.log('ok')
   })
-  .catch(err => {
-    console.log(err);
-  });
+  .catch((err) => {
+    console.log(err)
+  })
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,27 @@
+const { user } = require('assert');
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn = req.get('Cookie')
-        .split(';')[1] //dipende da quanti cookie inviamo, bisogna selezionare quello giusto
-        .trim()
-        .split('=')[1] == 'true';
-    res.render('auth/login', {
-        path: '/login',
-        pageTitle: 'Login',
-        isAuthenticated: req.isLoggedIn
-    });
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false,
+  });
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly'); //'HttpOnly' protegge da tentativi di XSS; 'Secure' permette di utilizzare il cookie solo con Https
-    res.redirect('/');
+  User.findById('5baa2528563f16379fc8a610')
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch((err) => console.log(err));
 };
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
+  };
