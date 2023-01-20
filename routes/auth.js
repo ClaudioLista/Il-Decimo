378a85e0af8ba3cbebd2958066a875d807aa49ce
Ciro Marrazzo <ciromarrazzo@me.com>
2023-01-20 15:45:34 +0100

    Fix (really) auth

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ var FacebookStrategy = require('passport-facebook')
 
 router.get('/login', isLog, authController.getLogin)
 
-router.get('/https://ildecimo.it/login/federated/google', passport.authenticate('google', {
+router.get('/login/federated/google', passport.authenticate('google', {
     scope: [
       'https://www.googleapis.com/auth/userinfo.profile',
       'https://www.googleapis.com/auth/userinfo.email'
@@ -24,17 +24,17 @@ router.get('/https://ildecimo.it/login/federated/google', passport.authenticate(
   })
 )
 
-router.get('https://ildecimo.it/login/federated/facebook', passport.authenticate('facebook', {
+router.get('/login/federated/facebook', passport.authenticate('facebook', {
   scope:['email']
 }))
 
-router.get('https://ildecimo.it/oauth2/redirect/google', passport.authenticate('google', {
+router.get('/oauth2/redirect/google', passport.authenticate('google', {
     successRedirect: '/',
     failureRedirect: '/login'
   })
 )
 
-router.get('https://ildecimo.it/oauth2/redirect/facebook', passport.authenticate('facebook', {
+router.get('/oauth2/redirect/facebook', passport.authenticate('facebook', {
     successRedirect: '/',
     failureRedirect: '/login'
   })
@@ -98,7 +98,7 @@ passport.use(
       clientID:
         '368248899535-vgc9fj94cfkk9sojps8pct6bjgu2d4j0.apps.googleusercontent.com',
       clientSecret: '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',
-      callbackURL: 'https://ildecimo.it/oauth2/redirect/google',
+      callbackURL: '/oauth2/redirect/google',
       scope: ['profile']
     },
     function verify(issuer, profile, cb) {
@@ -146,7 +146,7 @@ passport.use(
     {
       clientID: '846393583262405',
       clientSecret: 'b0bdd9d16238229f41cdd8f7bc5ab6c6',
-      callbackURL: 'https://ildecimo.it/oauth2/redirect/facebook',
+      callbackURL: '/oauth2/redirect/facebook',
       state: true,
       profileFields: ['id', 'email', 'gender', 'name'],
     },
