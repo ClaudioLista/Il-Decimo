aa3de93b637f20c079cad93923fb0be59b0999aa
Ciro Marrazzo <cir.marrazzo@studenti.unina.it>
2023-02-03 18:51:50 +0100

    Fix for render deploy

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ vault().then((data) => {
       cookie: {
         maxAge: 7200000, //la sessione si cancella dopo 2h
         httpOnly: true,
-        secure: true  //solo in fase di deploy
+        //secure: true  //solo in fase di deploy
       },
     })
   );
